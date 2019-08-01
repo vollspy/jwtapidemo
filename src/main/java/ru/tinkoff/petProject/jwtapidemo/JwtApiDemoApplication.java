43b0dc1d20d4dc9917402b221c989ca08ab16909
vollspy <tolyavetoshkin1337@gmail.com>
2019-08-02 00:15:38 +0500

    my 1st jwtAuth proj

---
+++ src/main/java/ru/tinkoff/petProject/jwtapidemo/JwtApiDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.petProject.jwtapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/tinkoff/petProject/jwtapidemo/JwtApiDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.tinkoff.petProject.jwtapidemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JwtApiDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
