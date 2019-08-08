c2fa993611beb61ff8ccb61c4fd5ce6c6d6e03f1
Vetoshkin Anatoliy Andreevich <a.vetoshkin@tinkoff.ru>
2019-08-08 12:50:49 +0500

    Jwt security

---
+++ src/main/java/ru/tinkoff/petProject/jwtapidemo/security/JwtUserDetailsService.java
@@ -0,0 +1,30 @@
+package ru.tinkoff.petProject.jwtapidemo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import ru.tinkoff.petProject.jwtapidemo.model.User;
+import ru.tinkoff.petProject.jwtapidemo.service.UserService;
+
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Autowired
+    public JwtUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User with username: " + username + " not found");
+        }
+
+        return null;
+    }
+}
--- src/main/java/ru/tinkoff/petProject/jwtapidemo/service/impl/UserServiceImpl.java
+++ src/main/java/ru/tinkoff/petProject/jwtapidemo/service/impl/UserServiceImpl.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.tinkoff.petProject.jwtapidemo.model.Role;
+import ru.tinkoff.petProject.jwtapidemo.model.Status;
 import ru.tinkoff.petProject.jwtapidemo.model.User;
 import ru.tinkoff.petProject.jwtapidemo.repository.RoleRepository;
 import ru.tinkoff.petProject.jwtapidemo.repository.UserRepository;
 import ru.tinkoff.petProject.jwtapidemo.service.UserService;
 
+import javax.jws.soap.SOAPBinding;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,28 +38,47 @@ public class UserServiceImpl implements UserService {
         Role roleUser = roleRepository.findByName("ROLE_USER");
         List<Role> userRoles = new ArrayList<>();
         userRoles.add(roleUser);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(userRoles);
+        user.setStatus(Status.ACTIVE);
 
+        User registeredUser = userRepository.save(user);
 
-        return null;
+        log.info("IN register - user: {} seccessfully registered", registeredUser);
+
+        return registeredUser;
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        List<User> result = userRepository.findAll();
+        log.info("IN getAll - {} user found", result.size());
+        return result;
     }
 
     @Override
     public User findByUsername(String username) {
-        return null;
+        User result = userRepository.findByUsername(username);
+        log.info("IN findByUsername - user: {} found by username: {}", result, username);
+        return result;
     }
 
     @Override
     public User findById(Long id) {
-        return null;
+        User result = userRepository.findById(id).orElse(null);
+
+        if (result == null) {
+            log.warn("IN findById - no user found by id: {}", id);
+            return null;
+        }
+
+        log.info("IN findById - user: {} found by id: {}", result);
+        return result;
     }
 
     @Override
     public void delete(Long id) {
-
+        userRepository.deleteById(id);
+        log.info("IN delete - user with id: {} seccesfully deleted");
     }
 }
